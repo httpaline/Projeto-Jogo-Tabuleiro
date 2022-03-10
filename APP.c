0daf319d03b5ff560fd79c3f1fd7ee9cb832932f
SabrinaDG <80282075+SabrinaDG@users.noreply.github.com>
2022-03-10 11:35:37 -0300

    Update APP.c

--- APP.c
+++ APP.c
@@ -5,7 +5,26 @@ int main()
 {
 
     //Tabuleiro
+    printf("\nTabuleiro do jogo: \n");
     tabuleiro();
+
+    //Primeira fasse
+
+        printf("\nComandos da primeira fase:\n");
+        ComandosPF();
+
+
+
+    //Segunda fase
+
+        printf("\nComandos da segunda fase:\n");
+        ComandosSF();
+
+
+    //Terceira fase
+
+    printf("\nComandos da terceira fase:\n");
+    ComandosTF();
 
     return 0;
 }
